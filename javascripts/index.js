6f855da8cf87e24e8eb6e4376d2344db00c41eb5
Surayah Holt <+>
2023-05-11 15:33:44 -0400

    Title Made

--- javascripts/index.js
+++ javascripts/index.js
@@ -4,7 +4,7 @@ const baseURL = 'http://localhost:3000/cards/'
 //grab elements of document
 const main = document.getElementById('main')
 const viewCollection = document.getElementById('view')
-const newCard = document.getElementById('newCard')
+const newCard = document.getElementById('newPup')
 
 
 //content load and page refresh
@@ -18,147 +18,119 @@ function pagerefresh(){
 }
 
 //event listeners
-viewCollection.addEventListener('click', fetchCards)
-newCard.addEventListener('click', createNewCard)
+viewCollection.addEventListener('click', fetchPups)
+newCard.addEventListener('click', createNewApopt)
 
 
-function fetchCards(){
+function fetchPups(){
     fetch (baseURL)
     .then (res=>res.json())
     .then (pagerefresh())
-    .then(data=>data.forEach(card=>renderOneCard(card)))    
+    .then(data=>data.forEach(pup=>renderOnePup(pup)))    
 }
 
 //function to renderOneCard
-function renderOneCard(cardObj){
-    const card = document.createElement('ul')
-    card.id = `${cardObj.id}`
-    card.className='card'
-    card.innerHTML=`
-    <img src="${cardObj.image}" class="card-pic" />
-    <div class="card-info">
-        <p>${cardObj.name}</p>
-        <p> ${cardObj.cardNumber}</p>
-        <p> ${cardObj.edition}</p>
-        <p>
-        CurrentBid: $<span class="current-bid">${cardObj.currentBid}</span>
-        </p>
-    <div class="card-buttons">
-        <button id ="bid" class="waves-effect waves-light btn indigo darken-3">Bid $5 </button>
-        <button id ="buyout" class="waves-effect waves-light btn red accent-4">Purchase For:$<span class="current-bid">${cardObj.price}</span></button>
+function renderAdoptPup(pupObj){
+    const pup = document.createElement('ul')
+    card.id = `${pupObj.id}`
+    card.className='pup'
+    pup.innerHTML=`
+    <img src="${pupObj.image}" class="pup-pic" />
+    <div class="pup-info">
+        <p>${pupObj.name}</p>
+        <p> ${pupObj.pupAge}</p>
+        <p> ${pupObj.breed}</p>
+        
     `
-    main.appendChild(card)
-    card.querySelector('#bid').addEventListener('click', ()=>{
-        cardObj.currentBid +=5
-        card.querySelector('span').textContent = cardObj.currentBid
-        updateBids(cardObj)
-    })
-    card.querySelector('#buyout').addEventListener('click', (event)=>{
-        console.log(event)
-        card.remove()
-        deleteCard(cardObj)
-    })
+   
 }
 
 
 //function to submit new card
-function createNewCard(){
+function createNewAdopt(){
     pagerefresh()
     const form = document.createElement('form')
-    form.id = 'addCardForm'
+    form.id = 'addPupForm'
     form.innerHTML= `
-        <h3>Submit your own card for Auction</h3>
+        <h3>Info for our new guest!</h3>
         <input
           type="text"
-          name="name"
+          name="breed"
           value=""
-          placeholder="Enter Card Name..."
+          placeholder="Enter Dog Breed..."
           class="input-text"
         />
         <br />
         <input
           type="text"
-          name="image"
+          name="age"
           value=""
-          placeholder="Enter Card Image URL..."
+          placeholder="Enter Dog Age..."
           class="input-text"
         />
         <br />
         <input
           type="text"
-          name="edition"
+          name="Name"
           value=""
-          placeholder="Enter Edition..."
+          placeholder="Enter Dogs Name..."
           class="input-text"
         />
         <br />
         <input
           type="text"
-          name="cardNumber"
+          name="favorite food"
           value=""
-          placeholder="Enter Card Number..."
+          placeholder="Enter Dogs Favorite Food..."
           class="input-text"
         />
         <br />
         <input
           type="text"
-          name="price"
+          name="temperment"
           value=""
-          placeholder="Enter Buyout Price..."
+          placeholder="Enter Dog Temperment..."
           class="input-text"
         />
         <br />
-        <button id ="submitCard" class="waves-effect waves-light btn blue accent-1">Submit Card </button>
+        <button id ="submitPup" class="waves-effect waves-light btn blue accent-1">Submit Pup </button>
     `
     main.appendChild(form)
-    document.querySelector('form').addEventListener('submit', newCardObj)
+    document.querySelector('form').addEventListener('submit', newPupObj)
 }
 
-function newCardObj(e){
+function newPupObj(e){
     e.preventDefault()
-    let newCardObj={
+    let newPupObj={
+        breed:e.target.breed.value,
         name:e.target.name.value,
-        image:e.target.image.value,
-        edition:e.target.edition.value,
-        cardNumber:e.target.cardNumber.value,
-        price:e.target.price.value,
-        currentBid: 0        
+        favoriteFood:e.target.favoriteFood.value,
+        dogTemperment:e.target.dogTemperment.value,      
     }
-    renderOneCard(newCardObj)
-    postNewCard(newCardObj)
+    renderOnePup(newPupObj)
+    postNewPup(newPupObj)
     document.querySelector('form').reset()
 }
 
 //funtion to post new cards to db
-function postNewCard(newCardObj){
+function postNewPup(newPupObj){
     fetch (baseURL,{
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
-      body:JSON.stringify(newCardObj)
+      body:JSON.stringify(newPupObj)
     })
     .then(res=>res.json())
-    .then(card=>console.log(card))
+    .then(pup=>console.log(pup))
   }
 
 
 //function to update bids to db
-function updateBids(cardObj){
-    fetch(`${baseURL}${cardObj.id}`,{
-        method: 'PATCH',
-        headers:{
-            "Content-Type": 'application/json'
-        },
-        body:JSON.stringify(cardObj)
-    })
-    .then(res=>res.json())
-    .then(card=>console.log(card))
-}
 
 // function to delete card from db
-function deleteCard(cardObj){
-    fetch(`${baseURL}${cardObj.id}`,{
+function goingHome(cardObj){
+    fetch(`${baseURL}${pupObj.id}`,{
         method: 'DELETE',
         headers:{
             'Content-Type':'application/json'
@@ -170,20 +142,3 @@ function deleteCard(cardObj){
 
 //Countdown function to end Auction
 //function borrowed from w3schools.com
-function startCountdown(){
-    const countDownDate = new Date("Jan 31, 2022 00:00:00").getTime();
-    const id = setInterval(function() {
-        const now = new Date().getTime();
-        const distance = countDownDate - now;
-        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-        document.getElementById("timer").innerHTML = days + "d " + hours + "h "
-        + minutes + "m " + seconds + "s ";
-        if (distance < 0) {
-            clearInterval(id);
-            document.getElementById("timer").innerHTML = "EXPIRED";
-        }
-    }, 1000);
-}
